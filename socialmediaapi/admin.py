14dc750f2224cfcfb5d516ad6ece06f0c4fc2a25
kumarkekse <pgoyal@bestpeers.com>
2020-10-07 20:12:32 +0530

    second commit

--- socialmediaapi/admin.py
+++ socialmediaapi/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Comment, Follower, Post, PostRate, Profile, TimeStampMixin
 
-# Register your models here.
+admin.site.register(TimeStampMixin)
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Follower)
+admin.site.register(PostRate)
+admin.site.register(Comment)
--- socialmediaapi/models.py
+++ socialmediaapi/models.py
@@ -1,3 +1,119 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_currentuser.db.models import CurrentUserField
+from django_currentuser.middleware import get_current_authenticated_user
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    GENDER_CHOICES = (
+        ('F', 'Female',),
+        ('M', 'Male',),
+    )
+    gender = models.CharField( max_length=1,
+        choices=GENDER_CHOICES,)
+    date_of_birth = models.DateField()
+    contact_no = models.CharField(max_length=10)
+    bio = models.CharField(max_length=100, null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    profile_picture = models.ImageField(upload_to="profile-pictures", null=True, blank=True)
+
+    def get_user_id(self):
+        return self.user.pk
+
+    def get_username(self):
+        return self.user.username
+    
+    def get_first_name(self):
+        return self.user.first_name
+    
+    def get_last_name(self):
+        return self.user.last_name
+
+    def get_followers_count(self):
+        return Follower.objects.filter(user = self.user).exclude(is_followed_by = self.user).count()
+
+    def get_following_count(self):
+        return Follower.objects.filter(is_followed_by = self.user).count()
+
+    def get_profile_belongs_to_authenticated_user(self):
+        return self.user == get_current_authenticated_user()
+
+    def __str__(self):
+        return str(self.user)
+
+
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)    
+
+
+class Post(TimeStampMixin):
+    caption = models.CharField(max_length=200)
+    post_by = CurrentUserField(related_name='posted_by')
+    image = models.ImageField(upload_to='post-images', null=True)
+
+    def get_post_belongs_to_authenticated_user(self):
+        return self.posted_by.pk == get_current_authenticated_user().pk
+
+    def get_user(self):
+        user_dict = vars(self.posted_by)
+        return {"id": user_dict["id"], "username": user_dict["username"]}
+    
+    def get_likes_count(self):
+        return PostRate.objects.filter(liked=True, rated_post=self).count()
+
+    def get_dislikes_count(self):
+        return PostRate.objects.filter(liked=False, rated_post=self).count()
+
+
+    def comments_count(self):
+        comments = Comment.objects.filter(post=self).count()
+        return comments
+
+    def __str__(self):
+        return str(self)
+
+
+class Follower(models.Model): 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    is_followed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='is_followed_by')
+
+    def get_user_info(self):
+        user_dict = vars(self.user)
+        return {"id": user_dict["id"], "username": user_dict["username"]}
+
+    def get_is_followed_by_info(self):
+        user_dict = vars(self.is_followed_by)
+        return {"id": user_dict["id"], "username": user_dict["username"]}
+
+    def get_following(self, user):
+        return Follower.objects.filter(is_followed_by=user)
+
+    def get_followers(self, user):
+        return Follower.objects.filter(user=user).exclude(is_followed_by=user)
+
+    def get_following_count(self, user):
+        return Follower.objects.filter(is_followed_by=user).count()
+
+    def get_followers_count(self, user):
+        return Follower.objects.filter(user=user).count()
+
+    def __str__(self):
+        return str(self)
+
+
+class Comment(TimeStampMixin):
+    content = models.TextField()
+    commented_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class PostRate(models.Model):
+    liked = models.BooleanField(null=True)
+    rated_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    rated_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.rated_post)
+
